7f7cff83e9426c0dc2908aad0343e216e57e37a8
Mark Pimentel <markp@quickplay.com>
2014-01-16 13:49:58 -0500

    - Added support for centos, rhel platforms

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,7 +10,7 @@ default['android-sdk']['version']        = '22.2.1'
 default['android-sdk']['components']     = %w(platform-tools build-tools-18.0.1 android-18 sysimg-18 android-17 sysimg-17 android-16 sysimg-16 extra-android-support extra-google-google_play_services extra-google-m2repository extra-android-m2repository)
 
 case node['platform']
-  when 'ubuntu'
+  when 'ubuntu', 'centos', 'redhat'
     default['android-sdk']['profile']        = "/etc/profile.d/#{node['android-sdk']['name']}.sh"
     default['android-sdk']['download_url']   = "http://dl.google.com/android/android-sdk_r#{node['android-sdk']['version']}-linux.tgz"
     default['android-sdk']['checksum']       = '17ea4610008985eb1454b3b25ad00c59300cee3141f7ea3deec98f45d862faa5'
